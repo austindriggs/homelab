a38bb17b719fd6d4babf13345d30594899fdea2c
austindriggs <austind26@icloud.com>
2025-07-29 22:04:07 -0400

    ntfy notifications for raid status!

---
+++ driggs-zb/main/ntfy.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import subprocess
+import requests
+import sys
+
+NTFY_URL = "http://driggs-zb:8047"
+NTFY_TOPIC = "sys"
+
+def send_ntfy(title, message, priority, tags=None, click=None, delay=None):
+	headers = {
+		"Title": title,
+		"Priority": str(priority),
+	}
+
+	if tags: headers["Tags"] = tags
+	if click: headers["Click"] = click
+	if delay: headers["Delay"] = delay
+
+	requests.post(
+		f"{NTFY_URL}/{NTFY_TOPIC}",
+		data=message.encode("utf-8"),
+		headers=headers
+	)
+
+def check_raid(frequency):
+	status = subprocess.run(
+			["mdadm", "--detail", "/dev/md0"],
+			capture_output=True, text=True, check=True
+		).stdout
+
+	for line in status.splitlines():
+		if line.strip().startswith("Failed Devices :"):
+			failed = int(line.split(":")[1].strip())
+			if failed > 0:
+				send_ntfy(f"RAID FAILED: {failed}", status, 5)
+			elif frequency == "weekly":
+				send_ntfy(f"RAID PASSED: {failed}", status, 3)
+			else: return
+
+if __name__ == "__main__":
+	frequency = sys.argv[1]
+	check_raid(frequency)
